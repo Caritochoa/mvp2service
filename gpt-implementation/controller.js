69a240d716f010a8e44a5393127168818ff026bc
carolina de Avellaneda <karittochoa@gmail.com>
2023-07-19 17:11:59 -0500

    implementation of mvc pattern

---
+++ gpt-implementation/controller.js
@@ -0,0 +1,4 @@
+// controller.js
+exports.getIndex = (req, res) => {
+  res.send("¡hola mundo te sa,uda el controlador !");
+};
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
+const routes = require("./gpt-implementation/routes");
 
 // Ruta de ejemplo
-app.get("/", (req, res) => {
-  res.send("¡hola hola hola hola !");
-});
+app.use("/", routes);
 
 // Puerto en el que se ejecutará el servidor
 const port = 3000;
